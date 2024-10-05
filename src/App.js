ba96012d7783856120d98b5ab23f1e1ce0ee18a0
ashbrooke8 <ashleybhunchak@gmail.com>
2024-10-05 15:55:57 -0600

    making the clouds move

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-import mountain from './Images/mountain.png';
-
+import clouds from './Images/clouds.png';
+import mountain from './Images/mountainsolo.png';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
-  return(
+  return (
     <div className="App">
-    <img src={mountain} className="full-page-image" alt="background" />
-  </div>
-  )
+      <div className="background">
+        <img src={clouds} className="clouds-background" alt="clouds" />
+        <img src={clouds} className="clouds-dupe" alt="clouds" />
+      </div>
+      <img src={mountain} className="mountain" alt="mountain" />
+    </div>
+  );
 }
 
 export default App;
