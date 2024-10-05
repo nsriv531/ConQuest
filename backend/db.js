0c559af20767ba87e95f0a67984e93d6714f0578
daraakitoyemru <dakit711@mtroyal.ca>
2024-10-05 12:49:15 -0600

    added db name

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'postgres',         // default superuser unless you created a different one
-  host: 'localhost',        // or the remote IP/host of your PostgreSQL server
-  database: 'your_database_name',
-  password: 'your_password', // the password you set during PostgreSQL installation
-  port: 5432,               // default PostgreSQL port
+  user: "postgres", // default superuser unless you created a different one
+  host: "localhost", // or the remote IP/host of your PostgreSQL server
+  database: "postgres",
+  password: "", // the password you set during PostgreSQL installation
+  port: 5432, // default PostgreSQL port
 });
 
 module.exports = pool;
