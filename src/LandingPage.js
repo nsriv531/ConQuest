a3a6649e093cbe0d55e17e4d154542073e0c18b6
ashbrooke8 <ashleybhunchak@gmail.com>
2024-10-05 22:12:56 -0600

    landing page css

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,6 +1,10 @@
 import './LandingPage.css';
 import clouds from './Images/clouds-brighter.png';
 import mountain from './Images/mountainsolo.png';
+import notification_bar from './Assets/notification_text.png';
+import landing_banner from './Assets/conQuest_logo_with_banner.png';
+import button_left from './Assets/login_button.png';
+import button_right from './Assets/signup_button.png';
 
 function App() {
   return (
@@ -10,6 +14,12 @@ function App() {
         <img src={clouds} className="clouds-dupe" alt="clouds" />
       </div>
       <img src={mountain} className="mountain" alt="mountain" />
+            <img src={notification_bar} className="notification-bar" alt="notification bar" />
+      <img src={landing_banner} className="landing-banner" alt="landing banner" />
+      <div className="button-container">
+        <img src={button_left} className="button-left" alt="left button" />
+        <img src={button_right} className="button-right" alt="right button" />
+      </div>
       </div>
   );
 }
