36f979a1b36815f761d6c5fa3431788b60d02668
nsriv531 <nsriv531@mtroyal.ca>
2024-10-05 23:09:14 -0600

    sdfsf

---
+++ src/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './HomePage.css';  // Import the CSS file
+
+function HomePage() {
+  return (
+    <div className="homepage-container">
+    </div>
+  );
+}
+
+export default HomePage;
