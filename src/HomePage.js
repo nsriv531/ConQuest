a9bd05060b15e72f62c2c58eca9e1af049b2bb89
nsriv531 <nsriv531@mtroyal.ca>
2024-10-06 01:32:21 -0600

    dashboard stuff and other things

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import './HomePage.css';  // Import the CSS file
+import TaskForm from './Forms/TaskForm';
+import LogoutButton from './Forms/LogoutButton';
+import ShopButton from './Forms/ShopButton';
+import AccountIcon from './Forms/AccountIcon';
+import ProjetIcon from './Forms/ProjectIcon';
+import DashBoardInterface from './Forms/DashBoardInterface';
+
 
 function HomePage() {
   return (
     <div className="homepage-container">
+      <div className="content-wrapper">
+        <AccountIcon />
+        <ShopButton />  
+        <TaskForm />  
+        <DashBoardInterface />  
+      </div>
     </div>
   );
 }
